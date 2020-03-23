cefa12efbe6090527707c4c058daa523fc0786ec
FranciscoRZ <franciscoreyzaluar@gmail.com>
2020-03-23 18:11:16 +0100

    Clean up file headers

--- StrategyBacktesterAddin/DataWriter.cs
+++ StrategyBacktesterAddin/DataWriter.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 
-using DataImporter;
-
 namespace StrategyBacktesterAddin
 {
     public static class DataWriter
